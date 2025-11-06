3ce63f500d8b31d7d9d1b03c0b137b1af087ae97
Priscila Anjos <priscilaanjos@infojr.com.br>
2025-11-05 21:48:37 -0300

    clear

--- .next/dev/static/chunks/app_page_b687ecf6.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_bfcf96ac._.js",
-  "static/chunks/app_simulator_components_9cad30b6._.css"
-],
-    source: "dynamic"
-});
